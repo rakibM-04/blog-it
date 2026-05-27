76b5640d81ff96be915fc07e036e6664851e2536
Rakib Mondal <rakibmondal2155@gmail.com>
2026-05-27 12:28:37 +0530

    Setup Tailwind CSS

---
+++ db/migrate/20260526113457_post.rb
@@ -0,0 +1,13 @@
+class Post < ActiveRecord::Migration[8.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :upvotes, null: false, default: 0
+      t.integer :downvotes, null: false, default: 0
+      t.boolean :is_bloggable, default: false
+
+      t.timestamps
+    end
+  end
+end
