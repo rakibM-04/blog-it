e932860193dcf8ee4cb2776dc5c6108e3207cb7f
Rakib Mondal <rakibmondal2155@gmail.com>
2026-05-27 13:45:48 +0530

    Setup Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
   content: ["./app/javascript/**/*.{js,jsx}", "./app/views/**/*.html.erb"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+};
